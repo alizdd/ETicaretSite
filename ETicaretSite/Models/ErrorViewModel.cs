6983157517935869e9b0fbf7006b435da9851019
ali.ozdede.idea <ali.ozdede.idea@gmail.com>
2021-11-13 23:53:40 +0300

    Add project files.

---
+++ ETicaretSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ETicaretSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
